52e6722276bdc2cfbfdfe10865c7a282a6dc39d2
Zaur <zaurik87@gmail.com>
2024-11-07 09:50:00 +0400

    Create Login and Home controllers

---
+++ Customer/src/main/java/com/ecommerce/customer/controller/LoginController.java
@@ -0,0 +1,62 @@
+package com.ecommerce.customer.controller;
+
+import com.ecommerce.library.dto.CustomerDto;
+import com.ecommerce.library.model.Customer;
+import com.ecommerce.library.service.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final CustomerService customerService;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("title", "Login page");
+        model.addAttribute("page", "Home");
+        return "login";
+    }
+
+    @GetMapping("/do-register")
+    public String registerCustomer(@Valid @ModelAttribute("customerDto") CustomerDto customerDto,
+                                   BindingResult bindingResult,
+                                   Model model) {
+        try {
+            if (bindingResult.hasErrors()) {
+                model.addAttribute("customerDto", customerDto);
+                return "register";
+            }
+            String username = customerDto.getUsername();
+            Customer customer = customerService.findByUsername(username);
+            if (customer != null) {
+                model.addAttribute("customerDto", customerDto);
+                model.addAttribute("error", "Email has been register!");
+                return "register";
+            }
+            if (customerDto.getPassword().equals(customerDto.getConfirmPassword())) {
+                customerDto.setPassword(passwordEncoder.encode(customerDto.getPassword()));
+                customerService.save(customerDto);
+                model.addAttribute("success", "Register successfully");
+            } else {
+                model.addAttribute("error", "Password is incorrect");
+                model.addAttribute("customerDto", customerDto);
+                return "register";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("error", "Server is error, try again later");
+        }
+        return "register";
+    }
+}
